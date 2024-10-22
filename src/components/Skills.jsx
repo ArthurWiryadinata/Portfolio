529a0672f2892c5b60ac46c247a75a19acd6a865
Arthur <arthur.wiryadinata@binus.edu>
2024-10-22 20:17:45 +0700

    Your commit message describing the changes

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -11,7 +11,7 @@ import MySql from '../assets/mysql.png';
 
 const Skills = () => {
   return (
-    <div className="bg-black text-gray-400 md:h-[150px] max-w-[1200px] mx-auto grid grid-cols-4 place-items-center md:flex md:justify-between md:items-center">
+    <div className="bg-black text-gray-400 md:h-[150px] max-w-[1200px] mx-auto grid grid-cols-4 place-items-center md:flex md:justify-between md:items-center  mt-12">
       <h2 className="text-gray-700 text-2xl md:text-4xl font-bold m-4">
         My <br /> Tech <br />
       </h2>
@@ -50,12 +50,6 @@ const Skills = () => {
       <div className="flex flex-col items-center m-4 w-[80px] md:w-[100px]">
         <img src={reactPng} alt="React JS" className="w-[60px] h-[60px] object-contain" />
         <p className="mt-2">React JS</p>
-      </div>
-
-      {/* Tailwind */}
-      <div className="flex flex-col items-center m-4 w-[80px] md:w-[100px]">
-        <img src={tailPng} alt="Tailwind" className="w-[60px] h-[60px] object-contain" />
-        <p className="mt-2">Tailwind</p>
       </div>
 
       {/* Figma */}
